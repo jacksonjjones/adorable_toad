7d9e82231f9db9b8091777fe589aaac0a287c748
jacksonjjones <jacksonjjones1@gmail.com>
2024-05-07 10:04:00 -0500

    added code to app.jsx, header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css';
+import React, { useState } from "react";
+import Header from "../src/components/Header";
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
+import Footer from "../src/components/Footer";
+import "./App.css";
 
 function App() {
+  const [currentTab, handleTabChange] = useState("about");
+
+  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
   return (
-    <div>
-   
-    </div>
+    <>
+      <title>Jackson Jones | {currentTab} </title>
+      <Header
+        currentTab={currentTab}
+        handleTabChange={handleTabChange}
+      ></Header>
+      <main>{renderTab()}</main>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Nav from "../Nav";
+
+function Header(props) {
+  const { currentTab, handleTabChange } = props;
+
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Jackson Jones</h1>
+          </div>
+          <div>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
