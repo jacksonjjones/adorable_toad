6585ad538b0f4ee9e4d04e3c9f5ac4b37a1fb545
jacksonjjones <jacksonjjones1@gmail.com>
2024-05-12 20:16:58 -0500

    added code to content and portfolio pages. added the majority of styling. added a email validation helper js. also imported screenshots for portfolio page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Header from "../src/components/Header/Header";
 import About from "../src/components/About/About";
-// import Contact from "../src/components/Contact/Contact";
-// import Portfolio from "../src/components/Portfolio/Portfolio";
+import Contact from "../src/components/Contact/Contact";
+import Portfolio from "../src/components/Portfolio/Portfolio";
 // import Resume from "../src/components/Resume/Resume";
 import Footer from "../src/components/Footer/Footer";
 import "./App.css";
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import onlineBookshelfImage from "../assets/onlineBookshelf.png"
+import techBlogImage from "../assets/techBlog.png"
+import fillMyBellyImage from "../assets/fillMyBelly.png"
+import weatherDashboardImage from "../assets/weatherDashboard.png"
+import jsQuizImage from "../assets/jsQuiz.png"
+import workDaySchedulerImage from "../assets/workDayScheduler.png"
+ 
+
+const projects = [
+  {
+    id: 0,
+    title: "Online Bookshelf",
+    image: onlineBookshelfImage,
+    repo: "https://github.com/jacksonjjones/online_bookshelf",
+    live: "https://online-bookshelf-3a41511a672a.herokuapp.com/",
+  },
+  {
+    id: 1,
+    title: "The Tech Blog",
+    image: techBlogImage,
+    repo: "https://github.com/jacksonjjones/exotic_butter",
+    live: "https://exotic-butter-ae43edb28af4.herokuapp.com/",
+  },
+  {
+    id: 2,
+    title: "Fill My Belly",
+    image: fillMyBellyImage, 
+    live: "https://jacksonjjones.github.io/Fill_My_Belly/",
+    repo: "https://github.com/jacksonjjones/Fill_My_Belly",
+  },
+  {
+    id: 3,
+    title: "Weather dashboard",
+    image: weatherDashboardImage, 
+    repo: "https://github.com/jacksonjjones/exciting_potato",
+    live: "https://jacksonjjones.github.io/exciting_potato/",
+  },
+  {
+    id: 4,
+    title: "JavaScript Quiz",
+    image: jsQuizImage, 
+    repo: "https://github.com/jacksonjjones/functional_rock",
+    live: "",
+  },
+  {
+    id: 5,
+    title: "Work Day Scheduler",
+    image: workDaySchedulerImage, 
+    repo: "https://github.com/jacksonjjones/amused_tree",
+    live: "https://jacksonjjones.github.io/amused_tree/",
+  },
+];
+
+function Portfolio() {
+  return (
+    <div>
+      <header className="projectsHeader">
+        <h2>My Projects</h2>
+      </header>
+      <div className="portfolio-container">
+        <div className="project-grid">
+          {projects.map((project) => (
+            <div className="project-card" key={project.id}>
+              <div className="card">
+                <div className="card-image">
+                  <figure className="card-actual-image">
+                    <a>
+                      <img src={project.image || "https://via.placeholder.com/300"} alt={project.title} />
+                    </a>
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <div className="card-title">
+                    <div className="other-card-content">
+                      <p className="title is-4">{project.title}</p>
+                    </div>
+                  </div>
+
+                  </div>
+                </div>
+                <footer className="card-footer">
+                  <a
+                    href={project.repo}
+    className="card-footer-item button-link" 
+    target="_blank"
+    rel="noreferrer"
+  >
+    Repo
+  </a>
+  <a
+    href={project.live}
+    className="card-footer-item button-link" 
+    target="_blank"
+    rel="noreferrer"
+  >
+    Live Site
+  </a>
+</footer>
+
+              </div>
+          
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Portfolio;
